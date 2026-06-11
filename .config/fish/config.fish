d9e752da6cb127f266be7ca2813216e88a026df5
04amanrajj <04.aman.raj@gmail.com>
2026-06-11 14:27:21 +0530

    Add fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set -g fish_greeting
+end
+
+# Created by `pipx` on 2026-02-02 08:54:40
+set PATH $PATH /home/amanrajj/.local/bin
+
+
+# Added by Antigravity CLI installer
+set -gx PATH "/home/amanrajj/.local/bin" $PATH
